7f60c48a95c23b4f9438abf756214f3dea9b38ca
BloxBoss6 <bloxboss6@gmail.com>
2020-09-16 17:01:03 -0400

    Riptide able to edit signs.

---
+++ src/main/java/bloxboss6/mod/objects/items/RiptideSignEditor.java
@@ -0,0 +1,13 @@
+package bloxboss6.mod.objects.items;
+
+import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class RiptideSignEditor {
+
+    @SubscribeEvent
+    public void riptideEditSign(RightClickBlock event) {
+
+    }
+
+}
