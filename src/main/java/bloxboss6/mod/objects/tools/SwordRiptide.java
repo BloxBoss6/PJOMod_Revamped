12929bdfc91e4ffd2c88fdb5c78d9070cc16e8cb
BloxBoss6 <bloxboss6@gmail.com>
2020-10-05 22:43:32 -0400

    Lots of stuff

--- src/main/java/bloxboss6/mod/objects/tools/SwordRiptide.java
+++ src/main/java/bloxboss6/mod/objects/tools/SwordRiptide.java
@@ -1,12 +1,14 @@
 package bloxboss6.mod.objects.tools;
 
+import bloxboss6.mod.Main;
 import bloxboss6.mod.init.ItemInit;
+import bloxboss6.mod.util.interfaces.IHasModel;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 
-public class SwordRiptide extends Item/*ItemSword implements IHasModel*/ {
+public class SwordRiptide extends Item implements IHasModel {
 
     public SwordRiptide(String name, CreativeTabs tab) {
         setUnlocalizedName(name);
@@ -25,6 +27,11 @@ public class SwordRiptide extends Item/*ItemSword implements IHasModel*/ {
     public void registerModels(ModelRegistryEvent event) {
         ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation("pjom:sword_riptide_new", "inventory"));
     }*/
+
+    @Override
+    public void registerModels() {
+        Main.proxy.registerItemRenderer(this, 0, "inventory");
+    }
 
 
 
